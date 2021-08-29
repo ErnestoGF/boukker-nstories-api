72acdb3308d38ab8f402230ef81c9fb8f47b79d8
Vallin Plasencia Valdes <vallin.plasencia@gmail.com>
2021-08-29 16:11:01 -0400

    Fix module name

--- grpc/go/proto-generator.sh
+++ grpc/go/proto-generator.sh
@@ -1,4 +1,6 @@
 echo "Generating files..."
 mkdir -p ./v2/storiespb
-# protoc -I ../proto/ --proto_path=../proto/third_party ../proto/stories.proto --go_out=plugins=grpc:./v2
-protoc -I=../proto/ --go_opt=paths=source_relative --go_out=v2/storiespb ../proto/stories.proto
+protoc -I=../proto/ \
+--go_opt=paths=source_relative --go_out=v2/storiespb \
+--go-grpc_opt=paths=source_relative --go-grpc_out=v2/storiespb \
+../proto/stories.proto
--- grpc/go/v2/go.mod
+++ grpc/go/v2/go.mod
@@ -1,9 +1,9 @@
-module github.com/ErnestoGF/boukker-stories-api/grpc/go/v2
+module github.com/ErnestoGF/boukker-nstories-api/grpc/go/v2
 
-go 1.13
+go 1.16
 
 require (
-	github.com/ErnestoGF/boukker-commons/go/v2 v2.0.0-alpha.7
-	google.golang.org/grpc v1.26.0
+	github.com/ErnestoGF/boukker-commons/go/v2 v2.1.1
+	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 )
