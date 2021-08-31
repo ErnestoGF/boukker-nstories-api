158decc288babbcf335b82f651fe0968307d5b13
Vallin Plasencia Valdes <vallin.plasencia@gmail.com>
2021-08-30 23:38:09 -0400

    new status validation story

--- grpc/go/v2/go.mod
+++ grpc/go/v2/go.mod
@@ -4,8 +4,6 @@ go 1.16
 
 require (
 	github.com/ErnestoGF/boukker-commons/go/v2 v2.1.2-0.20210829201657-1fff29f50344
-	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
-	github.com/stretchr/objx v0.1.1 // indirect
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
 )
--- grpc/go/v2/status/status.go
+++ grpc/go/v2/status/status.go
@@ -11,6 +11,38 @@ const (
 
 	// CodeChapterNotFound ...
 	CodeChapterNotFound
+
+	// CodeErrorUserLoggedWrong ...
+	CodeErrorUserLoggedWrong
+
+	// CodeErrorValidateAudience ...
+	CodeErrorValidateAudience
+	// CodeErrorValidateCategoryID ...
+	CodeErrorValidateCategoryID
+	// CodeErrorValidateCharacter ...
+	CodeErrorValidateCharacter
+	// CodeErrorValidateClasification ...
+	CodeErrorValidateClasification
+	// CodeErrorValidateCommentsModeration ...
+	CodeErrorValidateCommentsModeration
+	// CodeErrorValidateCopyright ...
+	CodeErrorValidateCopyright
+	// CodeErrorValidateAuthor ...
+	CodeErrorValidateAuthor
+	// CodeErrorValidateLanguageID ...
+	CodeErrorValidateLanguageID
+	// CodeErrorValidateStatus ...
+	CodeErrorValidateStatus
+	// CodeErrorValidateTags ...
+	CodeErrorValidateTags
+	// CodeErrorValidateTag ...
+	CodeErrorValidateTag
+	// CodeErrorValidateTitle ...
+	CodeErrorValidateTitle
+	// CodeErrorValidateTokenCover ...
+	CodeErrorValidateTokenCover
+	// CodeErrorValidateDescription ...
+	CodeErrorValidateDescription
 )
 
 var (
@@ -18,19 +50,130 @@ var (
 	StatusStoryNotFound = &commonsStatus.Status{
 		CodeGRPC: codes.NotFound,
 		Code:     CodeStoryNotFound,
-		Message:  "Story not found",
+		Message:  "story not found",
 	}
 	// StatusChapterNotFound ...
 	StatusChapterNotFound = &commonsStatus.Status{
 		CodeGRPC: codes.NotFound,
 		Code:     CodeChapterNotFound,
-		Message:  "Chapter not found",
+		Message:  "chapter not found",
+	}
+	// StatusErrorUserLoggedWrong ...
+	StatusErrorUserLoggedWrong = &commonsStatus.Status{
+		CodeGRPC: codes.FailedPrecondition,
+		Code:     CodeErrorUserLoggedWrong,
+		Message:  "user not login or user id incorrect",
+	}
+
+	// ================ validation story ================ //
+
+	// StatusErrorValidateAudience ...
+	StatusErrorValidateAudience = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateAudience,
+		Message:  "audience is invalid",
+	}
+	// StatusErrorValidateCategoryID invalid user ID
+	StatusErrorValidateCategoryID = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateCategoryID,
+		Message:  "category id is required",
+	}
+	// StatusErrorValidateCharacter ...
+	StatusErrorValidateCharacter = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateCharacter,
+		Message:  "the number the characters allowed for the character name is between 1 and 50",
+	}
+	// StatusErrorValidateClasification ...
+	StatusErrorValidateClasification = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateClasification,
+		Message:  "invalid clasification",
+	}
+	// StatusErrorValidateCommentsModeration ...
+	StatusErrorValidateCommentsModeration = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateCommentsModeration,
+		Message:  "comments moderation is invalid",
+	}
+	// StatusErrorValidateCopyright ...
+	StatusErrorValidateCopyright = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateCopyright,
+		Message:  "copyright is invalid",
+	}
+	// StatusErrorValidateAuthor ...
+	StatusErrorValidateAuthor = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateAuthor,
+		Message:  "author is required because story no is original",
+	}
+	// StatusErrorValidateLanguageID ...
+	StatusErrorValidateLanguageID = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateLanguageID,
+		Message:  "language id is required",
+	}
+	// StatusErrorValidateStatus ...
+	StatusErrorValidateStatus = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateStatus,
+		Message:  "status is invalid",
+	}
+	// StatusErrorValidateTags ...
+	StatusErrorValidateTags = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateTags,
+		Message:  "tags length allowed is between 1 and 20",
+	}
+	// StatusErrorValidateTag ...
+	StatusErrorValidateTag = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateTag,
+		Message:  "the number the characters allowed for the tag name is between 1 and 50",
+	}
+	// StatusErrorValidateTitle ...
+	StatusErrorValidateTitle = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateTitle,
+		Message:  "the number the characters allowed for the title is between 1 and 150",
+	}
+	// StatusErrorValidateTokenCover ...
+	StatusErrorValidateTokenCover = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateTokenCover,
+		Message:  "token cover is invalid",
+	}
+	// StatusErrorValidateDescription ...
+	StatusErrorValidateDescription = &commonsStatus.Status{
+		CodeGRPC: codes.InvalidArgument,
+		Code:     CodeErrorValidateDescription,
+		Message:  "the number the characters allowed for the description is between 1 and 250",
 	}
 )
 var codeStatus = map[commonsStatus.Code]*commonsStatus.Status{
 
-	CodeStoryNotFound:   StatusStoryNotFound,
-	CodeChapterNotFound: StatusChapterNotFound,
+	CodeStoryNotFound:        StatusStoryNotFound,
+	CodeChapterNotFound:      StatusChapterNotFound,
+	CodeErrorUserLoggedWrong: StatusErrorUserLoggedWrong,
+
+	// ==============================
+
+	CodeErrorValidateAudience:           StatusErrorValidateAudience,
+	CodeErrorValidateCategoryID:         StatusErrorValidateCategoryID,
+	CodeErrorValidateCharacter:          StatusErrorValidateCharacter,
+	CodeErrorValidateClasification:      StatusErrorValidateClasification,
+	CodeErrorValidateCommentsModeration: StatusErrorValidateCommentsModeration,
+	CodeErrorValidateCopyright:          StatusErrorValidateCopyright,
+	CodeErrorValidateAuthor:             StatusErrorValidateAuthor,
+	CodeErrorValidateLanguageID:         StatusErrorValidateLanguageID,
+	CodeErrorValidateStatus:             StatusErrorValidateStatus,
+	CodeErrorValidateTags:               StatusErrorValidateTags,
+	CodeErrorValidateTag:                StatusErrorValidateTag,
+	CodeErrorValidateTitle:              StatusErrorValidateTitle,
+	CodeErrorValidateTokenCover:         StatusErrorValidateTokenCover,
+	CodeErrorValidateDescription:        StatusErrorValidateDescription,
 }
 
 // FromCode ...
